f6862724c4132c1f967e29be55443d899c4e1e76
Mokunfope <kunfope.akinyemi@gmail.com>
2025-09-04 14:12:52 +0100

    Your commit message here

--- src/pages/signup/Email.tsx
+++ src/pages/signup/Email.tsx
@@ -10,7 +10,7 @@ function EmailConfirmation() {
     <div className="email-confirmation-container">
       <div className="email-confirmation-content">
         <div className="email-confirmation-icon"></div>
-        <Link to="/login" className="email-confirmation-title">
+        <Link to="" className="email-confirmation-title">
           Check your inbox!
         </Link>
         <p className="email-confirmation-message">
@@ -28,7 +28,7 @@ function EmailConfirmation() {
           Open Gmail
         </a>
         <p className="email-confirmation-signin">
-          <Link to="/"> Already have an account? Sign In</Link>
+          <Link to="/login"> Already have an account? Sign In</Link>
         </p>
       </div>
     </div>
